a6e2da3611f33c264ff76f3fef544bb3ee2ca0ac
Unknown <Kan_30489@hotmail.com>
2018-01-13 13:48:39 +0700

    ใบงานที่8.2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 int main () {
 
-        char name[] = {"Anukul"};
+        char name[] = {"kanjana"};
         int nameLength = 6;
         cout << name;
-
+        char nom[8];
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
-
-
+        nom[0] = name[6];
+        nom[1] = name[5];
+        nom[2] = name[4];
+        nom[3] = name[3];
+        nom[4] = name[2];
+        nom[5] = name[1];
+        nom[6] = name[0];
+        nom[7] = '\0';
+        cout << nom;
 
         return 0;
 }
